2aa992a3a90c0c6904f7b3fe6a7a191210b66dc3
Harish Reddy <harish.palugulla225@gmail.com>
2025-05-31 01:05:04 -0700

    remove crap code

--- backend/app.py
+++ backend/app.py
@@ -225,7 +225,3 @@ def ping():
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
- git config --global user.email "harish.palugulla225@gmail.com"
-  git config --global user.name "Harish Reddy"
